3f2883cb07ac2c9b3f742343dd2099735fb77901
Patrick Eugene de Farias <30466561+patrickfarias@users.noreply.github.com>
2022-12-01 13:57:07 -0300

    ESR-23-16

--- src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -12,13 +12,15 @@ import lombok.Getter;
 public class AuthUser extends User {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	//private Long companyId;
 	private Long userId;
 	private String fullName;
 	
 	public AuthUser(Usuario usuario) {
 		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
-		
+
+		//this.companyId = usuario.getId();
 		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
 	}
